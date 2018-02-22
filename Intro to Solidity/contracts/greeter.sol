f070aa8c0a9b992947e5aba6e4e8c3b6d43df168
Ryvikk <24513797+Ryvikk@users.noreply.github.com>
2018-02-22 13:04:32 -0800

    Update greeter.sol

--- Intro to Solidity/contracts/greeter.sol
+++ Intro to Solidity/contracts/greeter.sol
@@ -1,12 +1,19 @@
 pragma solidity 0.4.19;
 
-
 contract Greeter {
+
     string private greeting;
 
-    function Greeter(string _greeter) public {
+    function Greeter(string _greeting) {
+        greeting = _greeting;
     }
 
-    function greet() public view returns (string) {
+    function printG() public view returns (string) {
+        return greeting;
     }
+
+    function modifGreet(string _greeting) returns (string){
+        return greeting = _greeting;
+    }    
+
 }

254a7476410ef9e28ce765d3049ab52552869969
Ryvikk <24513797+Ryvikk@users.noreply.github.com>
2018-02-22 13:03:39 -0800

    Update fibonacci.sol

--- Intro to Solidity/contracts/fibonacci.sol
+++ Intro to Solidity/contracts/fibonacci.sol
@@ -1,12 +1,36 @@
 pragma solidity 0.4.19;
 
-
 contract Fibonacci {
-    /* Carry out calculations to find the nth Fibonacci number */
-    function fibRecur(uint n) public pure returns (uint) {
+	
+    function fibRec(uint number) returns(uint result) {
+        if (number <= 1) return number;
+        else return (fibRec(number - 1) + fibRec(number - 2));
     }
 
-    /* Carry out calculations to find the nth Fibonacci number */
-    function fibIter(uint n) public returns (uint) {
+    function fibIter(uint number) returns (uint result) {
+        uint i;
+        uint k;
+        uint l;
+        uint m;
+        if (number==0){
+            return 0;
+        }
+        else if (number==1){
+            return 1;
+        }
+        else{
+            k=0;
+            l=1;
+            for (i=2; i<=number; i++) {
+                m=k+l;
+                k=l;
+                l=m;
+            }
+        return l;
+        }
+
+
     }
+
+
 }
